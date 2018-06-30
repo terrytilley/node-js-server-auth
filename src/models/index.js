dcb45fafe17c9f0258f05d45ec21de533a46ad24
Terry Tilley <terry@terrytilley.com>
2018-06-30 01:16:22 +0100

    setup sequelize

---
+++ src/models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+
+const env = process.env.NODE_ENV || 'development';
+const { database, username, password } = require('../config/config.json')[env];
+
+const sequelize = new Sequelize(database, username, password, {
+  dialect: 'postgres',
+  operatorsAliases: Sequelize.Op,
+  define: { underscored: false },
+});
+
+const models = {
+  User: sequelize.import('./user'),
+};
+
+Object.keys(models).forEach(modelName => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import http from 'http';
 import app from './app';
+import models from './models';
 
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 server.close();
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+models.sequelize.sync({ force: true }).then(() => {
+  server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 });
