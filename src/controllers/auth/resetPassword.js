7057544171357ce722d83436ef9d69fe43079c20
Terry Tilley <terry@terrytilley.com>
2018-08-18 18:39:51 +0100

    reset password route

---
+++ src/controllers/auth/resetPassword.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt';
+import models from '../../models';
+
+const resetPassword = async ({ body: { password, token } }, res, next) => {
+  try {
+    const user = await models.User.find({ where: { resetToken: token } });
+
+    if (!user) {
+      return res.status(200).json({ message: 'User not found' });
+    }
+
+    const { id, resetExpires } = user;
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const currentTime = new Date(
+      new Date().setHours(new Date().getHours())
+    ).toISOString();
+
+    if (resetExpires.toString() > currentTime.toString()) {
+      await models.User.update(
+        {
+          resetToken: null,
+          resetExpires: null,
+          password: hashedPassword,
+        },
+        { where: { id } }
+      );
+    }
+
+    return res.status(200).json({ message: 'Password successfully reset' });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export default resetPassword;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,14 @@ import { requireLogin } from '../config/passport';
 
 import signUp from '../controllers/auth/signup';
 import signIn from '../controllers/auth/signIn';
+import resetPassword from '../controllers/auth/resetPassword';
 import forgotPassword from '../controllers/auth/forgotPassword';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signin', requireLogin, signIn);
+router.post('/reset-password', resetPassword);
 router.post('/forgot-password', forgotPassword);
 
 export default router;
